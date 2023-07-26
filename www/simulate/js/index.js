369702b84c4232ada7d7197ac172f234fa33ad08
Joshua Lees <joshualees@cedarville.edu>
2023-07-26 09:29:10 -0400

    commenting

--- www/simulate/js/index.js
+++ www/simulate/js/index.js
@@ -1,3 +1,8 @@
+/*
+    Main JavaScript file for simulator
+*/
+
+// Init side panel accordion
 $('.accordion').on('click', '.accordion-control', function(e){
     e.preventDefault(); //prevent default action of a button 
     $(this) //get the element the user clicked on
@@ -6,6 +11,7 @@ $('.accordion').on('click', '.accordion-control', function(e){
         .slideToggle(); //use slideToggle to show or hide it
 })
 
+// Component inner HTML
 let HTML = {
     'and':  '<div class="in-2"><div class="connector off" tabindex="1"></div><div class="connector off" tabindex="1"></div></div><div class="body and" tabindex="1"><img src="images/gates/AND.svg" draggable="true"></div><div class="connector off" tabindex="1"></div>',
     'or':   '<div class="in-2"><div class="connector off" tabindex="1"></div><div class="connector off" tabindex="1"></div></div><div class="body or" tabindex="1"><img src="images/gates/OR.svg" draggable="true"></div><div class="connector off" tabindex="1"></div>',
@@ -21,8 +27,10 @@ let HTML = {
     'led':  '<div class="body led low" tabindex="1" draggable="true"></div><div class="connector off" tabindex="1"></div>',
 }
 
+// Possible types of components
 let connTypes = ['gate', 'input', 'light']
 
+// Mapping of individual component names to their types
 let categories = {
     'and': connTypes[0],
     'or': connTypes[0],
@@ -38,42 +46,39 @@ let categories = {
     'led': connTypes[2],
 }
 
-
-
+// configuring data for pan feature
 let zoom = 0.065
 let yoff = document.querySelector("#navbar").getBoundingClientRect().height
 elementId = 0
-historyIdx = 0
-let components = {}
-let history = [{}]
-let dropzone = document.querySelector("#dropwindow")
 let scale = 1
-let selectedId = -1
+let components = {}
+let navMode = 1
+
+// for the simulator's event handling
+let dropzone = document.querySelector("#dropwindow")
 let pressedKeys = {}
-
 let mousedown = false
-
 let drawWire = false
 let wireOrigin = null
 
-let navMode = 1
-
+/* DISABLED: select box feature
 let selectBox = document.createElement('div')
 selectBox.id = 'selectbox'
 selectBox.style.position = 'absolute'
 selectBox.style.border = '2px solid rgb(227, 80, 80)'
 selectBox.style.backgroundColor = 'rgba(227, 80, 80, 0.2)'
 selectBox.style.borderRadius = '3px'
+*/
 
+// DOM object for simulation window
 let sim = document.querySelector("#simulation-window")
 let instance = panzoom(sim,{
     smoothScroll: false,
     zoomSpeed: zoom
 })
-
 instance.pause()
 
-
+// handler for dragging elements off of panel
 let panelDragstart = () => {
     let img = event.target
     let t = img.parentElement.id
@@ -83,6 +88,7 @@ let panelDragstart = () => {
     event.dataTransfer.setData('text/plain', JSON.stringify({from: 'panel', type: t, xoff: mx, yoff: my}))
 }
 
+/* DISABLED: handler for dragging elements once placed in simulation
 let simDragStart = () => {
     let img = event.target
     let id = ""
@@ -94,8 +100,11 @@ let simDragStart = () => {
     }
     event.dataTransfer.setData('text/plain', JSON.stringify({from: 'sim', type: id, xoff: 0, yoff: 0}))
 }
+*/
 
+// handler for swapping between pan and edit modes
 let updateMode = () => {
+    // pan
     if (navMode == 0) {
         document.querySelector('#pan-button').setAttribute('style','background-color:rgb(227, 80, 80);background-image:url("images/other/pan.svg");width:35px;height:35px;padding:0;margin:0;border-radius:5px')
         document.querySelector('#edit-button').setAttribute('style','background-color:none;background-image:url("images/other/cursor.svg");width:35px;height:35px;padding:0;margin:0;border-radius:5px')
@@ -105,6 +114,7 @@ let updateMode = () => {
             components[id].disableSelect()
         }
     }
+    // edit
     else {
         document.querySelector('#pan-button').setAttribute('style','background-color:none;background-image:url("images/other/pan.svg");width:35px;height:35px;padding:0;margin:0;border-radius:5px')
         document.querySelector('#edit-button').setAttribute('style','background-color:rgb(227, 80, 80);background-image:url("images/other/cursor.svg");width:35px;height:35px;padding:0;margin:0;border-radius:5px')
@@ -116,24 +126,15 @@ let updateMode = () => {
     }
 }
 
+// handler for undo button
 let undo = () => {
-    console.log(historyIdx)
-    if (historyIdx != 0) {
-        historyIdx -= 1
-        components = JSON.parse(JSON.stringify(history[historyIdx]))
-        refresh()
-    }
 }
 
+// handler for redo button
 let redo = () => {
-    console.log(historyIdx)
-    if (historyIdx != history.length - 1) {
-        historyIdx += 1
-        components = JSON.parse(JSON.stringify(history[historyIdx]))
-        refresh()
-    }
 }
 
+// handler for setting the mode (pan or edit)
 let mode = (m) => {
     if (navMode != m) {
         navMode = m
@@ -141,14 +142,17 @@ let mode = (m) => {
     }
 }
 
+// handler for saving the circuit to a .json file
 let save = () => {
     console.log('save')
 }
 
+// handler for loading a circuit from a .json file
 let load = () => {
     console.log('load')
 }
 
+// handler for removing all components from simulation
 let trash = () => {
     if(confirm("Are you sure you want to delete this circuit?")) {
         components = {}
@@ -157,24 +161,29 @@ let trash = () => {
     }
 }
 
+// handler for saving the currently viewable circuit to an image file
 let image = () => {
-    
 }
 
+// handler for opening the help window
 let help = () => {
     window.open('help.html', '_blank');
 }
 
+// hander for when the document loads
 $(function(){
+    // handler for confirming window reload or close
     window.onbeforeunload = function ()
     {
         return "";
     };
 
+    // setting the handler for draggable elements from the side panel
     for (let elem of document.querySelectorAll(".draggable")) {
         elem.addEventListener('dragstart', panelDragstart)
     }
 
+    // handler for deleting selected elements from the simulation
     document.body.addEventListener('keyup', () => {
         if (event.key === "Delete") {
             for (id of Object.keys(components)) {
@@ -186,12 +195,15 @@ $(function(){
         }
     })
 
+    // handlers for keeping track of which keys are being held down
     window.onkeyup = function(e) { pressedKeys[e.keyCode] = false; }
     window.onkeydown = function(e) { pressedKeys[e.keyCode] = true; }
 
+    // refreshes the mode after document loads
     updateMode()
 })
 
+// refreshes all components in the simulation window
 let refresh = () => {
     sim.innerHTML = ""
     for (id of Object.keys(components)) {
@@ -199,6 +211,7 @@ let refresh = () => {
     }
 }
 
+// for connect: gets the offsets of the connectors to be connected
 let  getOffset = (el) => {
     var rect = el.getBoundingClientRect();
     return {
@@ -209,6 +222,7 @@ let  getOffset = (el) => {
     };
 }
 
+// draws a wire between two connectors
 let connect = (div1, div2, color, thickness) => { // draw a line connecting elements
     var off1 = getOffset(div1);
     var off2 = getOffset(div2);
@@ -232,24 +246,27 @@ let connect = (div1, div2, color, thickness) => { // draw a line connecting elem
     var htmlLine = document.createElement('div')
     htmlLine.classList.add('wire')
     htmlLine.setAttribute('style',"z-index:-10;padding:0px; margin:0px; height:" + thickness + "px; background-color:" + color + "; line-height:1px; position:absolute; left:" + cx + "px; top:" + cy + "px; width:" + length + "px; -moz-transform:rotate(" + angle + "deg); -webkit-transform:rotate(" + angle + "deg); -o-transform:rotate(" + angle + "deg); -ms-transform:rotate(" + angle + "deg); transform:rotate(" + angle + "deg);");
-    //
-    // alert(htmlLine);
     sim.appendChild(htmlLine);
 }
 
+// disabling panning from the side panel
 document.querySelector("#side-panel").addEventListener('pointerdown', () => {
     instance.pause()
 })
 
+// listening for any click event on the document
 document.addEventListener('click', () => {
-
+    /*
     console.log(event.target)
     console.log(event.target.classList.value.includes('connector'))
+    */
 
+    // ensuring panning is always enabled when pan mode is enabled
     if (navMode == 0) {
         instance.resume()
     }
 
+    // deselects components only if (a) CTRL is not being held, and (b) the click is below the navigation bar
     if (!pressedKeys[17] && event.y > document.querySelector("#navbar").getBoundingClientRect().height) {
         for (id of Object.keys(components)) {
             if (event.target != components[id].getDom && event.target != components[id].getBody ) {
@@ -258,23 +275,28 @@ document.addEventListener('click', () => {
         }
     }
 
+    // if we did not click a connector, we do not want to begin the connetion process
     if (!event.target.classList.value.includes('connector')) {
         drawWire = false
         wireOrigin = null
     }
 
+    // if we did click a connector, begin the connection process
     else {
         if (drawWire) {
             if (event.target != wireOrigin) {
                 oP = wireOrigin
+                // find origin parent
                 while (!connTypes.some(el => oP.classList.value.includes(el))) {
                     oP = oP.parentElement
                 }
                 dP = event.target
+                // find destination parent
                 while (!connTypes.some(el => dP.classList.value.includes(el))) {
                     dP = dP.parentElement
                 }
 
+                // just so long as the connectors are not a part of the same component
                 if (oP != dP) {
                     // connect the connectors
                     if (wireOrigin.getBoundingClientRect().left < event.target.getBoundingClientRect().left) {
@@ -289,10 +311,12 @@ document.addEventListener('click', () => {
                 }
             }
         }
+        // begin connection process
         else {
             drawWire = true
             wireOrigin = event.target
         }
+        // deselect any components if a connector was clicked
         for (id of Object.keys(components)) {
             components[id].deselect()
         }
@@ -303,11 +327,14 @@ dropzone.addEventListener('dragover', () => {
     event.preventDefault()
 })
 
+// handler for dropping elements into the simulation window
 dropzone.addEventListener('drop', () => {
     event.preventDefault()
     dropData = JSON.parse(event.dataTransfer.getData("text"))
 
+    // if we dragged a component from the side panel
     if (dropData['from'] == 'panel') {
+        // if the component is a gate
         if (categories[dropData['type']] == 'gate') {
             let loc_x = ((event.x - sim.getBoundingClientRect().x) / scale) - dropData['xoff'] - 20
             let loc_y = (((event.y - yoff) - (sim.getBoundingClientRect().y - yoff)) / scale) - dropData['yoff']
@@ -321,6 +348,7 @@ dropzone.addEventListener('drop', () => {
             sim.appendChild(component)
             elementId += 1
         }
+        // if the component is an input
         else if (categories[dropData['type']] == 'input') {
             console.log('input',dropData['type'])
 
@@ -336,8 +364,10 @@ dropzone.addEventListener('drop', () => {
             sim.appendChild(component)
             elementId += 1
         }
+        // if the component is a light
         else if (categories[dropData['type']] == 'light') {
             console.log('light',dropData['type'])
+
         }
     }
     else {
@@ -345,6 +375,7 @@ dropzone.addEventListener('drop', () => {
     }
 })
 
+// handler for adjusting the scale veruable on any pan
 instance.on('transform', () => {
     scale = instance.getTransform().scale
 })
